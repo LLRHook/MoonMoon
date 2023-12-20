d7a5338ec6a4b194eebf5047ac044c9fb0bd6cec
Victor Ivanov <vivanov@paradigmtesting.com>
2023-12-20 10:26:03 -0500

    testing

--- main.py
+++ main.py
@@ -3,13 +3,9 @@ from tkinter import ttk
 
 def initialize():
     root = Tk()
-
-    frm = ttk.Frame(root, padding=10)
-    frm.grid()
-    ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
-    ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
-
+    root.title("Maze")
+    root.geometry("500x500")
     root.mainloop()
 
-if __name__ == "main":
+if __name__ == "__main__":
     initialize()
